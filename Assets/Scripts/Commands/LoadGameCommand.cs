1e3651e3f060805e1a2561b7196d7474cfdddcd2
OnurHG <onurhangumus@gmail.com>
2022-07-31 15:14:31 +0300

    -Load key control has added

--- Assets/Scripts/Commands/LoadGameCommand.cs
+++ Assets/Scripts/Commands/LoadGameCommand.cs
@@ -6,7 +6,8 @@ namespace Commands
     {
         public int OnLoadGameData(SaveLoadStates saveLoadStates)
         {
-            if (!ES3.FileExists()) return 0;
+           
+            if (!ES3.FileExists() || !ES3.KeyExists("Level") || !ES3.KeyExists("Money")) return 0;
             if (saveLoadStates == SaveLoadStates.Level) return ES3.Load<int>("Level");
             else if (saveLoadStates == SaveLoadStates.Money) return ES3.Load<int>("Money");
             else return 0;
