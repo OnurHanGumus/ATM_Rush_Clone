7f9b3622dc1f42d27657e053e01ce1db57992c8f
OnurHG <onurhangumus@gmail.com>
2022-07-31 14:46:33 +0300

    -LoadCommand'e if null kontrolu eklendi

--- Assets/Scripts/Commands/LoadGameCommand.cs
+++ Assets/Scripts/Commands/LoadGameCommand.cs
@@ -12,6 +12,7 @@ namespace Commands
     {
         public int OnLoadGameData(SaveLoadStates saveLoadStates)
         {
+            if (!ES3.FileExists()) return 0;
             if (saveLoadStates == SaveLoadStates.Level) return ES3.Load<int>("Level");
             else if (saveLoadStates == SaveLoadStates.Money) return ES3.Load<int>("Money");
             else return 0;
