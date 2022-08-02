b2d63360160bd366e26d17a4b0af6dad47009d8d
umutunlu96 <umutt.unlu.96@gmail.com>
2022-08-03 02:38:14 +0300

    Obstacle Anim Pigglet Anim

--- Assets/Scripts/Controllers/ObstacleAnimController.cs
+++ Assets/Scripts/Controllers/ObstacleAnimController.cs
@@ -1,4 +1,3 @@
-using System;
 using DG.Tweening;
 using UnityEngine;
 
@@ -6,24 +5,62 @@ namespace Controllers
 {
     public class ObstacleAnimController : MonoBehaviour
     {
-        [Header("DOTween")]
-        [SerializeField] private Ease _ease;
+        [Header("Bool")] 
+        [SerializeField] private bool doMove = true;
+        [SerializeField] private bool doShake = false;
+        [Space]
+        [Header("Move")][Space]
+        [SerializeField] private Ease easeStart = Ease.Linear;
+        [SerializeField] private Ease easeEnd = Ease.Linear;
+        [Header("Path")][Space]
         [SerializeField] private Vector3[] path = new Vector3[2];
-        [SerializeField] private float speed;
-        [SerializeField] private float delay;
+        [Header("Speed")][Space]
+        [SerializeField] private float speedStart = 1;
+        [SerializeField] private float speedEnd = 1;
+        [Header("Delay")]
+        [SerializeField] private float delay = 0;
+
+        [Header("Shake")] [Space] 
+        [SerializeField] private float shakeSpeed = 1;
+
         private Sequence _sequence;
+
+        private void Awake()
+        {
+            _sequence = DOTween.Sequence();
+        }
 
         private void Start()
         {
-            MoveAnimation();
+            
+            if(doMove)
+                MoveAnimation();
+            if(doShake)
+                ShakeAnim();
+            SetLoop();
         }
 
-        private void MoveAnimation()
+        public void MoveAnimation()
         { 
-            _sequence = DOTween.Sequence();
-            _sequence.Append(transform.DOMove(path[0], (1/speed), false).SetEase(_ease)
-                .SetDelay(delay)).Append(transform.DOMove(path[1], (1/speed), false).SetEase(_ease)
+            _sequence.Append(transform.DOMove(path[0], (1/speedStart), false).SetEase(easeStart)
+                .SetDelay(delay)).Append(transform.DOMove(path[1], (1/speedEnd), false).SetEase(easeEnd)
                 .SetDelay(delay));
+        }
+
+        private void ShakeAnim()
+        {
+            _sequence.Append(transform.DOShakePosition(.5f, .5f));
+        }
+        
+        #region Rotate
+        private void RotateAnim()
+        {
+            
+        }
+        #endregion
+        
+        private void SetLoop()
+        {
             _sequence.SetLoops(-1, LoopType.Restart);
         }
     }
