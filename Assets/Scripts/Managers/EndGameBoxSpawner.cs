642ba73e4b17150fa5befeba452d9b21c70e2552
umutunlu96 <umutt.unlu.96@gmail.com>
2022-08-03 22:21:51 +0300

    EndGame Box

--- Assets/Scripts/Managers/EndGameBoxSpawner.cs
+++ Assets/Scripts/Managers/EndGameBoxSpawner.cs
@@ -16,17 +16,6 @@ namespace Managers
         private int aradakiMesafe = 2;
         private float startAmount = 1f;
         private float increaseAmount = 0.1f;
-        // [SerializeField] private List<Color32> colorList = new List<Color32>();
-
-        // private void Awake()
-        // {
-        //     for (int i = 0; i < 10; i++)
-        //     {
-        //         Color32 randomColor = new Color32((byte)Random.Range(0, 255),(byte)Random.Range(0, 255),
-        //             (byte)Random.Range(0, 255),(byte)255);
-        //         colorList.Add(randomColor);
-        //     }
-        // }
 
         private void Start()
         {
@@ -37,12 +26,6 @@ namespace Managers
                 startPosition = new Vector3(0, startPosition.y + aradakiMesafe, startPosition.z);
                 startAmount += increaseAmount;
                 GameObject obj = Instantiate(prefab, startPosition, quaternion.identity);
-            
-                Renderer rend = obj.GetComponent<Renderer>();
-                Color32 randomColor = new Color32((byte)Random.Range(0, 255),(byte)Random.Range(0, 255),
-                    (byte)Random.Range(0, 255),(byte)255);
-                rend.material.color = randomColor;
-                
                 obj.transform.SetParent(transform);
                 obj.transform.GetChild(0).GetComponent<TextMeshPro>().text = 
                     startAmount.ToString("0.0") + " X";
